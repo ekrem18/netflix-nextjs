017b520284deb7d79724044176252c7c89539781
Ekrem <avekremyilmazturk@gmail.com>
2023-09-01 21:59:32 +0300

    apikey upd

--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -1,18 +1,26 @@
-import { getMovies } from '@/helpers/movieFunctions';
-import React from 'react'
-import MovieSection from './components/movieSection';
+import { getMovies } from "@/helpers/movieFunctions";
+import React from "react";
+import MovieSection from "./components/movieSection";
+import HeroSection from "./components/HeroSection";
 
-const Movies = async() => {
+const Movies = async () => {
   const movies = await getMovies("now_playing");
   console.log(movies);
-  return <>
-  <div className='px-4 md:px-12 mt-4'>
-  <MovieSection title="NOW PLAYING" type="now_playing" />
-  <MovieSection title="POPULAR" type="popular" />
-  <MovieSection title="TOP RATED" type="top_rated" />
-  <MovieSection title="UPCOMING" type="upcoming" />
-  </div>
-  </>
-}
+  return (
+    <>
+      <HeroSection
+        title={movies[0]?.title}
+        overview={movies[0]?.overview}
+        id={movies[0]?.id}
+      />
+      <div className="px-4 md:px-12 mt-4">
+        <MovieSection title="NOW PLAYING" type="now_playing" />
+        <MovieSection title="POPULAR" type="popular" />
+        <MovieSection title="TOP RATED" type="top_rated" />
+        <MovieSection title="UPCOMING" type="upcoming" />
+      </div>
+    </>
+  );
+};
 
-export default Movies
+export default Movies;
