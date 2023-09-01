c7c90dba3230b10ea25cd63de9dc5abd1c3d8fc5
Ekrem <avekremyilmazturk@gmail.com>
2023-09-01 12:03:30 +0300

    api

--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -1,9 +1,18 @@
+import { getMovies } from '@/helpers/movieFunctions';
 import React from 'react'
+import MovieSection from './components/movieSection';
 
-const Movies = () => {
-  return (
-    <div>Movies</div>
-  )
+const Movies = async() => {
+  const movies = await getMovies("now_playing");
+  console.log(movies);
+  return <>
+  <div className='px-4 md:px-12 mt-4'>
+  <MovieSection title="NOW PLAYING" type="now_playing" />
+  <MovieSection title="POPULAR" type="popular" />
+  <MovieSection title="TOP RATED" type="top_rated" />
+  <MovieSection title="UPCOMING" type="upcoming" />
+  </div>
+  </>
 }
 
 export default Movies
--- helpers/movie Functions.js
+++
@@ -1,6 +0,0 @@
-const API_KEY = process.env.TMDB_KEY
-
-export const getMovies= asyn(type) =>{
-const URL = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}`;
-const res = await fetch(URL)
-}
